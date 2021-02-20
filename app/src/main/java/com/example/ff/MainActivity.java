71dbe58a60f1206580d5e1e4eeb29c16a6ba561d
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-02-20 17:15:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ff/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.ff;
+
+
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.text.SpannableStringBuilder;
+import android.webkit.WebViewClient;
+
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener{
+//public class Test02_01 extends Activity implements OnClickListener{
+
+ //private final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+ //private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+ private EditText textUrl;
+ private Button buttonGo;
+ private WebView webview;
+
+ @Override protected void onCreate(Bundle icicle) {
+  super.onCreate(icicle);
+
+  setContentView(R.layout.activity_main);
+
+  //WebView webview = (WebView) findViewById(R.id.webView1);
+  webview = (WebView) findViewById(R.id.webView);
+
+
+
+  webview.getSettings().setJavaScriptEnabled(true);
+  webview.getSettings().setBuiltInZoomControls(true);
+
+  String pdfUrl = "https://www.data.jma.go.jp/fcd/yoho/data/jishin/kaisetsu_tanki_latest.pdf";
+  String url = "http://docs.google.com/gview?embedded=true&url=" + pdfUrl;
+
+  //webview.invalidate();
+  webview.loadUrl(url);
+
+  webview.setWebViewClient(new WebViewClient() {
+   boolean checkOnPageStartedCalled = false;
+
+   @Override
+   public void onPageStarted(WebView view, String url, Bitmap favicon) {
+    checkOnPageStartedCalled = true;
+   }
+
+   @Override
+   public void onPageFinished(WebView view, String url) {
+    if (checkOnPageStartedCalled) {
+     //pdfView.loadUrl(removePdfTopIcon);
+     //hideProgress();
+    } else {
+     showPdfFile(url);
+    }
+   }
+
+  });
+
+ }
+
+ private LinearLayout.LayoutParams createParam(int w, int h){
+  return new LinearLayout.LayoutParams(w, h);
+ }
+ public void onClick(View v) {
+  SpannableStringBuilder url = (SpannableStringBuilder)textUrl.getText();
+  webview.loadUrl(url.toString());
+ }
+
+ private void showPdfFile(final String urlString) {
+  //showProgress();
+  webview.invalidate();
+  webview.getSettings().setJavaScriptEnabled(true);
+  //webview.getSettings().setSupportZoom(true);
+  webview.loadUrl(urlString);
+  webview.setWebViewClient(new WebViewClient() {
+   boolean checkOnPageStartedCalled = false;
+
+   @Override
+   public void onPageStarted(WebView view, String url, Bitmap favicon) {
+    checkOnPageStartedCalled = true;
+   }
+
+   @Override
+   public void onPageFinished(WebView view, String url) {
+    if (checkOnPageStartedCalled) {
+     //webview.loadUrl(removePdfTopIcon);
+     //hideProgress();
+    } else {
+     showPdfFile(urlString);
+    }
+   }
+  });
+ }
+}
+/*
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+*/
